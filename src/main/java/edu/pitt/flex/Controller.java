97095212ca689605ef68bad90dc7c5f7bb72920f
Paolo Hidalgo <56983967+phidalgodiblasio@users.noreply.github.com>
2023-03-15 02:57:27 -0400

    Set up Spring Boot application
    Set up a basic hello world program with Spring Boot to serve as a starting point for the project.

---
+++ src/main/java/edu/pitt/flex/Controller.java
@@ -0,0 +1,12 @@
+package edu.pitt.flex;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+	public String index() {
+		return "Hello, world!";
+	}
+}
---
+++ src/test/java/edu/pitt/flex/FlexApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pitt.flex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
