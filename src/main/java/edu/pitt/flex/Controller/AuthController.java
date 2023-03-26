41076dfabfc05418efa2f0e0d85e32f24bf7e6b5
Paolo Hidalgo <56983967+phidalgodiblasio@users.noreply.github.com>
2023-03-26 14:05:00 -0400

    Added basic session management

--- src/main/java/edu/pitt/flex/Controller/AuthController.java
+++ src/main/java/edu/pitt/flex/Controller/AuthController.java
@@ -10,22 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.pitt.flex.DTO.LoginDTO;
 import edu.pitt.flex.DTO.UserDTO;
 import edu.pitt.flex.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin
 public class AuthController {
-    @Autowired
+	@Autowired
 	private UserService userService;
 
 	@PostMapping("/save")
-	public ResponseEntity<String> saveUser(@RequestBody UserDTO userDTO)
-	{ 
+	public ResponseEntity<String> saveUser(@RequestBody UserDTO userDTO) {
 		return userService.addUser(userDTO);
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<String> loginUser(@RequestBody LoginDTO loginDTO)
-	{
-		return userService.loginUser(loginDTO);
+	public ResponseEntity<String> loginUser(@RequestBody LoginDTO loginDTO, HttpServletRequest request) {
+		return userService.loginUser(loginDTO, request);
 	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<String> logOutUser(HttpServletRequest request) {
+		return userService.logoutUser(request);
+	}	
 }
