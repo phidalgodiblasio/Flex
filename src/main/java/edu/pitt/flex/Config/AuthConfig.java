20a3a4bf0f0f8ada5af0ffdea01c258e1713b284
Paolo Hidalgo <56983967+phidalgodiblasio@users.noreply.github.com>
2023-03-26 23:22:16 -0400

    Added authentication with interceptors

---
+++ src/main/java/edu/pitt/flex/Config/AuthConfig.java
@@ -0,0 +1,19 @@
+package edu.pitt.flex.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import edu.pitt.flex.Component.AuthInterceptor;
+
+@Configuration
+public class AuthConfig implements WebMvcConfigurer {
+    @Autowired
+    AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/get-id"); // Change this later
+    }
+}
