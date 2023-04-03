426a890dbf6b5edb4adaf6c27cd2a561369e94bb
Paolo Hidalgo <56983967+phidalgodiblasio@users.noreply.github.com>
2023-04-03 17:59:46 -0400

    Revert "Merge branch 'main' of https://github.com/phidalgodiblasio/Flex"
    This reverts commit d024d89f99117aae169290cbbd580895226808a9, reversing
    changes made to 13885c216e7912efc44599dc8c6532033c5ae6dd.

--- src/main/java/edu/pitt/flex/Service/WeightServiceImpl.java
+++ src/main/java/edu/pitt/flex/Service/WeightServiceImpl.java
@@ -1,7 +1,6 @@
 package edu.pitt.flex.Service;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,37 +22,34 @@ public class WeightServiceImpl implements WeightService {
     public ResponseEntity<String> addWeight(WeightDTO weightDTO, HttpServletRequest request){
         String body;
         HttpStatus status;
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        String stringDate = "" + year + month + day;
-        int date = Integer.parseInt(stringDate);
-
-
 
         User user = userRepository.findOneById((int)request.getSession().getAttribute("USER_ID"));
-        Weight lastWeight=user.getLastWeight();
-       
-        if(lastWeight!=null && lastWeight.getDate()==date){
-            lastWeight.setWeight(weightDTO.getWeight());
-        } else{
+
+        if(user == null){
+            body = "User not logged in";
+            status = HttpStatus.BAD_REQUEST;
+        }
+        else{
             Weight weight = new Weight(weightDTO.getId(), weightDTO.getDate(), weightDTO.getWeight());
 
             user.addWeight(weight);
-        }
-           
 
             body = "Weight has been added";
             status = HttpStatus.OK;
-        
+        }
         return new ResponseEntity<>(body, status);
     }
     
     public List<Weight> getWeights(HttpServletRequest request){
-        List<Weight> weights = new ArrayList<Weight>();
-        return weights;
-        
+        User user = userRepository.findOneById((int)request.getSession().getAttribute("USER_ID"));
+
+        if(user != null){
+           return user.getAllWeights();
+        }
+        else{
+            List<Weight> weights = new ArrayList<Weight>();
+            return weights;
+        }
 }
 
 }
