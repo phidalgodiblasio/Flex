22070f801ebf98c58feb542c73b8511a3b17e718
Paolo Hidalgo <56983967+phidalgodiblasio@users.noreply.github.com>
2023-04-03 18:21:38 -0400

    Fixed issues

--- src/main/java/edu/pitt/flex/Service/UserServiceImpl.java
+++ src/main/java/edu/pitt/flex/Service/UserServiceImpl.java
@@ -12,6 +12,7 @@ import edu.pitt.flex.Entity.User;
 import edu.pitt.flex.Repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
@@ -21,7 +22,8 @@ public class UserServiceImpl implements UserService {
     PasswordEncoder passwordEncoder;
 
     @Override
-    public ResponseEntity<String> addUser(UserDTO userDTO, HttpServletRequest request) {
+    public ResponseEntity<String> addUser(UserDTO userDTO, HttpServletRequest request)
+    {
         // Response body and status
         String body;
         HttpStatus status;
@@ -29,8 +31,7 @@ public class UserServiceImpl implements UserService {
         // Search to see if username is taken
         if (userRepository.findByUsername(userDTO.getUsername()) == null) {
             // Create new user and add to repository
-            User user = new User(userDTO.getId(), userDTO.getUsername(),
-                    this.passwordEncoder.encode(userDTO.getPassword()));
+            User user = new User(userDTO.getId(), userDTO.getUsername(), this.passwordEncoder.encode(userDTO.getPassword()));
             userRepository.save(user);
 
             // Add user id to sessions
@@ -39,26 +40,28 @@ public class UserServiceImpl implements UserService {
             // Update response to show success
             body = "Registration successful";
             status = HttpStatus.OK;
-        } else {
+        }
+        else {
             // Update response to show failure
             body = "Registration unsuccessful: username already taken";
             status = HttpStatus.BAD_REQUEST;
         }
-
+        
         // Return response
         return new ResponseEntity<>(body, status);
     }
 
     @Override
-    public ResponseEntity<String> loginUser(LoginDTO loginDTO, HttpServletRequest request) {
+    public ResponseEntity<String> loginUser(LoginDTO loginDTO, HttpServletRequest request) 
+    {
         // Response body and status
         String body;
         HttpStatus status;
-
+        
         // Search for user in db
         User user = userRepository.findByUsername(loginDTO.getUsername());
-
-        // If user found, authenticate password
+        
+        // If user found, authenticate password 
         if (user != null && passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
             // Add user id to sessions
             request.getSession().setAttribute("USER_ID", user.getId());
@@ -81,7 +84,7 @@ public class UserServiceImpl implements UserService {
         // Response body and status
         String body;
         HttpStatus status;
-
+        
         // Invalidate user session
         try {
             request.getSession().invalidate();
@@ -91,67 +94,7 @@ public class UserServiceImpl implements UserService {
             body = "Logout unsuccessful: " + e.getMessage();
             status = HttpStatus.BAD_REQUEST;
         }
-
-        // Return response
-        return new ResponseEntity<>(body, status);
-    }
-
-    @Override
-    public ResponseEntity<String> setIntakeGoal(UserDTO userDTO, HttpServletRequest request) {
-        String body;
-        HttpStatus status;
-
-        // TODO: look into sessions, first case is returning null, so this only works
-        // when userDTO has username
-        User currUser = userRepository.findOneById((int) request.getSession().getAttribute("USER_ID"));
-
-        // Search to see if username exists
-        if (userRepository.findByUsername(currUser.getUsername()) == null) {
-            // Update response to show failure
-            body = "Update unsuccessful: username not found";
-            status = HttpStatus.BAD_REQUEST;
-        } else {
-            // update intake goals all goals need to be set each time.
-            currUser.setCalGoal(userDTO.getCalGoal());
-            currUser.setCarbGoal(userDTO.getCarbGoal());
-            currUser.setFatGoal(userDTO.getFatGoal());
-            currUser.setProteinGoal(userDTO.getProteinGoal());
-
-            userRepository.save(currUser);
-
-            // Update response to show success
-            body = "Intake goals set";
-            status = HttpStatus.OK;
-        }
-
-        // Return response
-        return new ResponseEntity<>(body, status);
-    }
-
-    @Override
-    public ResponseEntity<String> setWeightGoal(UserDTO userDTO, HttpServletRequest request) {
-        String body;
-        HttpStatus status;
-
-        // TODO: look into sessions, first case is returning null, so this only works
-        // when userDTO has username
-        User currUser = userRepository.findOneById((int) request.getSession().getAttribute("USER_ID"));
-
-        // Search to see if username exists
-        if (userRepository.findByUsername(currUser.getUsername()) == null) {
-            // Update response to show failure
-            body = "Update unsuccessful: username not found";
-            status = HttpStatus.BAD_REQUEST;
-        } else {
-            // update weight goals
-            currUser.setWeightGoal(userDTO.getWeightGoal());
-            userRepository.save(currUser);
-
-            // Update response to show success
-            body = "Weight goal set";
-            status = HttpStatus.OK;
-        }
-
+        
         // Return response
         return new ResponseEntity<>(body, status);
     }
